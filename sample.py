a632abcce66745c17e9aa9902523b837eeaef8d7
Charan <srichu.kattamuru@gmail.com>
2021-07-28 12:39:08 +0530

    Changed the URL

--- sample.py
+++ sample.py
@@ -18,7 +18,7 @@ while True:
         frame_bytes = cv2.imencode(".png", frame)[1].tobytes()  # Convert image data to bytes, to send to the endpoint
         files = {"file": frame_bytes}
         payload = {"choice": "lips"}
-        response = requests.post("http://127.0.0.1:8000/upload-file/", files=files, params=payload)  # API Call
+        response = requests.post("http://127.0.0.1:8000/apply-makeup/", files=files, params=payload)  # API Call
         if response.status_code == 200:
             image_content = response.content  # Response from the API
             rec_image = np.array(Image.open(BytesIO(image_content)))  # Convert the bytes response to numpy array
